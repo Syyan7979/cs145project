0f808d822b788d10b7d5e48b02fbce3ad1276300
Kurt Ian Khalid Israel <kiisrael@up.edu.ph>
2022-06-03 23:49:57 +0800

    new timout condition for slowstart asssess

--- sender.py
+++ sender.py
@@ -41,9 +41,9 @@ def Protocol():
     seqNum = 0
 
     # Timer calculation related variables
-    estimate_time = 1 # intializing estimated RTT to 3, which will be updated accordingly once we get a sample RTT
+    estimate_time = 2 # intializing estimated RTT to 3, which will be updated accordingly once we get a sample RTT
     DevRTT = estimate_time/2 # initializing DevRTT to initial estimated RTT divided by 2, which will be updated accordingly once we get a sample RTT
-    timeout_interval = 1
+    timeout_interval = 2
     alpha = 0.125
     beta = 0.25
 
