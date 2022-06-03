526671ef0d1102fc9096e7b43e2eb0c72e355618
Kurt Ian Khalid Israel <kiisrael@up.edu.ph>
2022-06-04 02:23:08 +0800

    new data freq

--- sender.py
+++ sender.py
@@ -102,6 +102,7 @@ def Protocol():
                     slowStart = False
                     congestionAvoidance = True
                     counter = 0
+                if firstSuccess == False
                     timeout_interval *= 2
             elif ((slowStart == False) and (congestionAvoidance == True) and (stasis == False)):
                 MSS = MSS - 1
