9a148d299528d6d9276901dd7aaf4ec716e6dd39
Kurt Ian Khalid Israel <kiisrael@up.edu.ph>
2022-06-03 21:58:22 +0800

    new edits7

--- sender.py
+++ sender.py
@@ -44,7 +44,7 @@ def Protocol():
     # Timer calculation related variables
     estimate_time = 1 # intializing estimated RTT to 3, which will be updated accordingly once we get a sample RTT
     DevRTT = estimate_time/2 # initializing DevRTT to initial estimated RTT divided by 2, which will be updated accordingly once we get a sample RTT
-    timeout_interval = 1
+    timeout_interval = 3
     alpha = 0.125
     beta = 0.25
 
