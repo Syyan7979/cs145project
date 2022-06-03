43b15364ce86f2c0e9eb4e193c1603e145936a7a
Kurt Ian Khalid Israel <kiisrael@up.edu.ph>
2022-06-03 20:40:28 +0800

    created a tcp reno/tahoe hybrid mechanism

--- sender.py
+++ sender.py
@@ -28,16 +28,22 @@ def Protocol():
     transactionID = int(acceptMessage.decode())
     print(transactionID)
 
-    MSS = 1
+    # States
+    slowStart = True
+    congestionAvoidance = False
+
+    MSS = 20
+    cwnd = 1 * MSS
     startPos = 0
     timeoutCounter = 0
+    ssthresh = 100
 
     seqNum = 0
 
     # Timer calculation related variables
     estimate_time = 1 # intializing estimated RTT to 3, which will be updated accordingly once we get a sample RTT
     DevRTT = estimate_time/2 # initializing DevRTT to initial estimated RTT divided by 2, which will be updated accordingly once we get a sample RTT
-    timeout_interval = 3
+    timeout_interval = 5
     alpha = 0.125
     beta = 0.25
 
@@ -45,14 +51,13 @@ def Protocol():
         # Responsible for setting the timeout value of our socket
         clientSocket.settimeout(timeout_interval)
         print(seqNum)
-        print(timeout_interval)
+
         # z gets 1 whenever the currentPosition + the size of the payload is greater than the lenght of the whole data
-        z = 1 if startPos + MSS >= len(full_payload) else 0
-        if startPos + MSS >= len(full_payload):
+        z = 1 if startPos + cwnd >= len(full_payload) else 0
+        if startPos + cwnd >= len(full_payload):
             dataPacket = f"ID{uniqueID}SN{seqNum:07d}TXN{transactionID:07d}LAST{z}{full_payload[startPos:]}".encode("ascii")
         else:
-            endpos = startPos + MSS
-            print(endpos, type(endpos), type(startPos))
+            endpos = startPos + cwnd
             dataPacket = f"ID{uniqueID}SN{seqNum:07d}TXN{transactionID:07d}LAST{z}{full_payload[startPos:endpos]}".encode("ascii")
         clientSocket.sendto(dataPacket, (IP_address, receiverPort))
         start = time.time()
@@ -61,24 +66,30 @@ def Protocol():
             ackPacket = clientSocket.recvfrom(4096)[0].decode()
             end = time.time() # returns the time for when the
             ackNumber, checkSum = ParseAckMessage(ackPacket)
-            if (ackNumber == seqNum):
-                startPos += MSS
+
+            if (slowStart and !congestionAvoidance and ackNumber == seqNum):
+                startPos += cwnd
+                cwnd += MSS
                 seqNum += 1
+                if (cwnd >= ssthresh):
+                    slowStart = False
+                    congestionAvoidance = True
+            elif (!slowStart and congestionAvoidance and ackNumber == seqNum):
+                startPos += cwnd
+                cwnd = cwnd + int(MSS * (MSS/cwnd))
+                seqNum += 1
+
             SampleRTT = end - start
             DevRTT = ((1-beta) * DevRTT) + (beta*abs(SampleRTT-estimate_time))
             estimate_time = ((1-alpha) * estimate_time) + (alpha * SampleRTT)
             timeout_interval = estimate_time + (4*DevRTT)
-            if timeoutCounter == 0:
-                MSS *= 2
-            elif timeoutCounter == 1:
-                MSS += 1
+
         except socket.timeout:
-            if timeoutCounter == 0:
-                MSS = MSS // 2
-                MSS += 1
-            elif timeoutCounter == 1:
-                MSS -= 1
-            timeoutCounter += 1
+            ssthresh = cwnd//2
+            cwnd = 1 * MSS
+            if (!slowStart and congestionAvoidance):
+                slowStart = True
+                congestionAvoidance = False
 
 def ParseAckMessage(message):
     ackNumber = int(message[3:10])
