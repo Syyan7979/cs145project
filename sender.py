6469e18237f9bff18237d59c8a4ea38467dd21ad
Kurt Ian Khalid Israel <kiisrael@up.edu.ph>
2022-06-03 23:38:03 +0800

    new timout condition for slowstart asssess

--- sender.py
+++ sender.py
@@ -100,7 +100,7 @@ def Protocol():
             elif ((slowStart == False) and (congestionAvoidance == True) and (stasis == False)):
                 MSS = prevMSS
                 congestionAvoidance = False
-                stasis = True
+                slowStart = True
         print(f"elapsed time: {time.time()-sendStart}")
 
 def ParseAckMessage(message):
