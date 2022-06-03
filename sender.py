ef386b4983d7c88f1ad52d79aee388540eff2128
Kurt Ian Khalid Israel <kiisrael@up.edu.ph>
2022-06-03 21:09:48 +0800

    mss and timer updates

--- sender.py
+++ sender.py
@@ -33,7 +33,7 @@ def Protocol():
     slowStart = True
     congestionAvoidance = False
 
-    MSS = 64
+    MSS = 24
     cwnd = 1 * MSS
     startPos = 0
     timeoutCounter = 0
@@ -42,9 +42,9 @@ def Protocol():
     seqNum = 0
 
     # Timer calculation related variables
-    estimate_time = 3 # intializing estimated RTT to 3, which will be updated accordingly once we get a sample RTT
+    estimate_time = 4 # intializing estimated RTT to 3, which will be updated accordingly once we get a sample RTT
     DevRTT = estimate_time/2 # initializing DevRTT to initial estimated RTT divided by 2, which will be updated accordingly once we get a sample RTT
-    timeout_interval = 3
+    timeout_interval = 4
     alpha = 0.125
     beta = 0.25
 
